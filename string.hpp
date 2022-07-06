35e0fbb668122c8c65b864a1f98dfc480dec3928
Jonathan Torres <jonathantorres41@gmail.com>
2022-07-05 23:45:23 -0400

    Use trim_left and trim_right when trimming the whitespace in a string

--- string.hpp
+++ string.hpp
@@ -13,17 +13,9 @@ std::string to_lower(const std::string &s);
 std::string to_upper(const std::string &s);
 
 std::string trim_whitespace(const std::string &s) {
-    auto it = s.begin();
-    while (it != s.end() && std::isspace(*it)) {
-        it++;
-    }
+    std::string res = trim_left(s);
 
-    auto rev_it = s.rbegin();
-    while (rev_it.base() != it && std::isspace(*rev_it)) {
-        rev_it++;
-    }
-
-    return std::string(it, rev_it.base());
+    return trim_right(res);
 }
 
 std::string trim_right(const std::string &s) {

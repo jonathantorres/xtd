8ac6dc7d5898083c004ffdbea133eb2b3432b960
Jonathan Torres <jonathantorres41@gmail.com>
2022-09-17 16:43:36 -0400

    Add more tests

--- tests/string_test.cpp
+++ tests/string_test.cpp
@@ -2,9 +2,10 @@
 #include <catch2/catch_test_macros.hpp>
 #include <list>
 #include <string>
+#include <string_view>
 #include <vector>
 
-TEST_CASE("whitespace is trimmed from a string with trim_whitespace()") {
+TEST_CASE("whitespace is trimmed with trim_whitespace()") {
     std::string s1("foo");
     std::string s2(" bar");
     std::string s3("baz ");
@@ -12,15 +13,58 @@ TEST_CASE("whitespace is trimmed from a string with trim_whitespace()") {
     std::string s5("this is a string");
     std::string s6("");
 
-    REQUIRE(string::trim_whitespace(s1) == "foo");
-    REQUIRE(string::trim_whitespace(s2) == "bar");
-    REQUIRE(string::trim_whitespace(s3) == "baz");
-    REQUIRE(string::trim_whitespace(s4) == "this");
-    REQUIRE(string::trim_whitespace(s5) == "this is a string");
-    REQUIRE(string::trim_whitespace(s6) == "");
+    SECTION("using string lvalues") {
+        REQUIRE(string::trim_whitespace(s1) == "foo");
+        REQUIRE(string::trim_whitespace(s2) == "bar");
+        REQUIRE(string::trim_whitespace(s3) == "baz");
+        REQUIRE(string::trim_whitespace(s4) == "this");
+        REQUIRE(string::trim_whitespace(s5) == "this is a string");
+        REQUIRE(string::trim_whitespace(s6) == "");
+    }
+
+    SECTION("using string rvalues") {
+        REQUIRE(string::trim_whitespace(std::string("foo")) == "foo");
+        REQUIRE(string::trim_whitespace(std::string(" bar")) == "bar");
+        REQUIRE(string::trim_whitespace(std::string("baz ")) == "baz");
+        REQUIRE(string::trim_whitespace(std::string(" this ")) == "this");
+        REQUIRE(string::trim_whitespace(std::string("this is a string")) ==
+                "this is a string");
+        REQUIRE(string::trim_whitespace(
+                    std::string("   this   is another string    ")) ==
+                "this   is another string");
+        REQUIRE(string::trim_whitespace(std::string("")) == "");
+    }
+
+    SECTION("using string constants") {
+        REQUIRE(string::trim_whitespace("foo") == "foo");
+        REQUIRE(string::trim_whitespace(" bar") == "bar");
+        REQUIRE(string::trim_whitespace("baz ") == "baz");
+        REQUIRE(string::trim_whitespace(" this ") == "this");
+        REQUIRE(string::trim_whitespace("this is a string") ==
+                "this is a string");
+        REQUIRE(string::trim_whitespace("   this   is another string    ") ==
+                "this   is another string");
+        REQUIRE(string::trim_whitespace("") == "");
+    }
+
+    SECTION("using string views") {
+        std::string_view sv1("foo");
+        std::string_view sv2(" bar");
+        std::string_view sv3("baz ");
+        std::string_view sv4(" this ");
+        std::string_view sv5("this is a string");
+        std::string_view sv6("");
+
+        REQUIRE(string::trim_whitespace(sv1) == "foo");
+        REQUIRE(string::trim_whitespace(sv2) == "bar");
+        REQUIRE(string::trim_whitespace(sv3) == "baz");
+        REQUIRE(string::trim_whitespace(sv4) == "this");
+        REQUIRE(string::trim_whitespace(sv5) == "this is a string");
+        REQUIRE(string::trim_whitespace(sv6) == "");
+    }
 }
 
-TEST_CASE("whitespace is trimmed from a string with trim()") {
+TEST_CASE("whitespace is trimmed with trim()") {
     std::string s1("foo");
     std::string s2(" bar");
     std::string s3("baz ");
@@ -28,15 +72,58 @@ TEST_CASE("whitespace is trimmed from a string with trim()") {
     std::string s5("this is a string");
     std::string s6("");
 
-    REQUIRE(string::trim(s1) == "foo");
-    REQUIRE(string::trim(s2) == "bar");
-    REQUIRE(string::trim(s3) == "baz");
-    REQUIRE(string::trim(s4) == "this");
-    REQUIRE(string::trim(s5) == "this is a string");
-    REQUIRE(string::trim(s6) == "");
+    SECTION("using string lvalues") {
+        REQUIRE(string::trim_whitespace(s1) == "foo");
+        REQUIRE(string::trim_whitespace(s2) == "bar");
+        REQUIRE(string::trim_whitespace(s3) == "baz");
+        REQUIRE(string::trim_whitespace(s4) == "this");
+        REQUIRE(string::trim_whitespace(s5) == "this is a string");
+        REQUIRE(string::trim_whitespace(s6) == "");
+    }
+
+    SECTION("using string rvalues") {
+        REQUIRE(string::trim_whitespace(std::string("foo")) == "foo");
+        REQUIRE(string::trim_whitespace(std::string(" bar")) == "bar");
+        REQUIRE(string::trim_whitespace(std::string("baz ")) == "baz");
+        REQUIRE(string::trim_whitespace(std::string(" this ")) == "this");
+        REQUIRE(string::trim_whitespace(std::string("this is a string")) ==
+                "this is a string");
+        REQUIRE(string::trim_whitespace(
+                    std::string("   this   is another string    ")) ==
+                "this   is another string");
+        REQUIRE(string::trim_whitespace(std::string("")) == "");
+    }
+
+    SECTION("using string constants") {
+        REQUIRE(string::trim_whitespace("foo") == "foo");
+        REQUIRE(string::trim_whitespace(" bar") == "bar");
+        REQUIRE(string::trim_whitespace("baz ") == "baz");
+        REQUIRE(string::trim_whitespace(" this ") == "this");
+        REQUIRE(string::trim_whitespace("this is a string") ==
+                "this is a string");
+        REQUIRE(string::trim_whitespace("   this   is another string    ") ==
+                "this   is another string");
+        REQUIRE(string::trim_whitespace("") == "");
+    }
+
+    SECTION("using string views") {
+        std::string_view sv1("foo");
+        std::string_view sv2(" bar");
+        std::string_view sv3("baz ");
+        std::string_view sv4(" this ");
+        std::string_view sv5("this is a string");
+        std::string_view sv6("");
+
+        REQUIRE(string::trim_whitespace(sv1) == "foo");
+        REQUIRE(string::trim_whitespace(sv2) == "bar");
+        REQUIRE(string::trim_whitespace(sv3) == "baz");
+        REQUIRE(string::trim_whitespace(sv4) == "this");
+        REQUIRE(string::trim_whitespace(sv5) == "this is a string");
+        REQUIRE(string::trim_whitespace(sv6) == "");
+    }
 }
 
-TEST_CASE("right whitespace is trimmed from a string") {
+TEST_CASE("right whitespace is trimmed with trim_right()") {
     std::string s1("foo");
     std::string s2(" bar");
     std::string s3("baz ");
@@ -44,15 +131,52 @@ TEST_CASE("right whitespace is trimmed from a string") {
     std::string s5("this is a string");
     std::string s6("");
 
-    REQUIRE(string::trim_right(s1) == "foo");
-    REQUIRE(string::trim_right(s2) == " bar");
-    REQUIRE(string::trim_right(s3) == "baz");
-    REQUIRE(string::trim_right(s4) == " this");
-    REQUIRE(string::trim_right(s5) == "this is a string");
-    REQUIRE(string::trim_right(s6) == "");
+    SECTION("using string lvalues") {
+        REQUIRE(string::trim_right(s1) == "foo");
+        REQUIRE(string::trim_right(s2) == " bar");
+        REQUIRE(string::trim_right(s3) == "baz");
+        REQUIRE(string::trim_right(s4) == " this");
+        REQUIRE(string::trim_right(s5) == "this is a string");
+        REQUIRE(string::trim_right(s6) == "");
+    }
+
+    SECTION("using string rvalues") {
+        REQUIRE(string::trim_right(std::string("foo")) == "foo");
+        REQUIRE(string::trim_right(std::string(" bar")) == " bar");
+        REQUIRE(string::trim_right(std::string("baz ")) == "baz");
+        REQUIRE(string::trim_right(std::string(" this ")) == " this");
+        REQUIRE(string::trim_right(std::string("this is a string")) ==
+                "this is a string");
+        REQUIRE(string::trim_right(std::string("")) == "");
+    }
+
+    SECTION("using string constants") {
+        REQUIRE(string::trim_right("foo") == "foo");
+        REQUIRE(string::trim_right(" bar") == " bar");
+        REQUIRE(string::trim_right("baz ") == "baz");
+        REQUIRE(string::trim_right(" this ") == " this");
+        REQUIRE(string::trim_right("this is a string") == "this is a string");
+        REQUIRE(string::trim_right("") == "");
+    }
+
+    SECTION("using string views") {
+        std::string_view sv1("foo");
+        std::string_view sv2(" bar");
+        std::string_view sv3("baz ");
+        std::string_view sv4(" this ");
+        std::string_view sv5("this is a string");
+        std::string_view sv6("");
+
+        REQUIRE(string::trim_right(sv1) == "foo");
+        REQUIRE(string::trim_right(sv2) == " bar");
+        REQUIRE(string::trim_right(sv3) == "baz");
+        REQUIRE(string::trim_right(sv4) == " this");
+        REQUIRE(string::trim_right(sv5) == "this is a string");
+        REQUIRE(string::trim_right(sv6) == "");
+    }
 }
 
-TEST_CASE("left whitespace is trimmed from a string") {
+TEST_CASE("left whitespace is trimmed with trim_left()") {
     std::string s1("foo");
     std::string s2(" bar");
     std::string s3("baz ");
@@ -60,12 +184,49 @@ TEST_CASE("left whitespace is trimmed from a string") {
     std::string s5("this is a string");
     std::string s6("");
 
-    REQUIRE(string::trim_left(s1) == "foo");
-    REQUIRE(string::trim_left(s2) == "bar");
-    REQUIRE(string::trim_left(s3) == "baz ");
-    REQUIRE(string::trim_left(s4) == "this ");
-    REQUIRE(string::trim_left(s5) == "this is a string");
-    REQUIRE(string::trim_left(s6) == "");
+    SECTION("using string lvalues") {
+        REQUIRE(string::trim_left(s1) == "foo");
+        REQUIRE(string::trim_left(s2) == "bar");
+        REQUIRE(string::trim_left(s3) == "baz ");
+        REQUIRE(string::trim_left(s4) == "this ");
+        REQUIRE(string::trim_left(s5) == "this is a string");
+        REQUIRE(string::trim_left(s6) == "");
+    }
+
+    SECTION("using string rvalues") {
+        REQUIRE(string::trim_left(std::string("foo")) == "foo");
+        REQUIRE(string::trim_left(std::string(" bar")) == "bar");
+        REQUIRE(string::trim_left(std::string("baz ")) == "baz ");
+        REQUIRE(string::trim_left(std::string(" this ")) == "this ");
+        REQUIRE(string::trim_left(std::string("this is a string")) ==
+                "this is a string");
+        REQUIRE(string::trim_left(std::string("")) == "");
+    }
+
+    SECTION("using string constants") {
+        REQUIRE(string::trim_left("foo") == "foo");
+        REQUIRE(string::trim_left(" bar") == "bar");
+        REQUIRE(string::trim_left("baz ") == "baz ");
+        REQUIRE(string::trim_left(" this ") == "this ");
+        REQUIRE(string::trim_left("this is a string") == "this is a string");
+        REQUIRE(string::trim_left("") == "");
+    }
+
+    SECTION("using string views") {
+        std::string_view sv1("foo");
+        std::string_view sv2(" bar");
+        std::string_view sv3("baz ");
+        std::string_view sv4(" this ");
+        std::string_view sv5("this is a string");
+        std::string_view sv6("");
+
+        REQUIRE(string::trim_left(sv1) == "foo");
+        REQUIRE(string::trim_left(sv2) == "bar");
+        REQUIRE(string::trim_left(sv3) == "baz ");
+        REQUIRE(string::trim_left(sv4) == "this ");
+        REQUIRE(string::trim_left(sv5) == "this is a string");
+        REQUIRE(string::trim_left(sv6) == "");
+    }
 }
 
 TEST_CASE("string is converted to lower case") {
@@ -75,11 +236,48 @@ TEST_CASE("string is converted to lower case") {
     std::string s4("THIS iS aNoTHer STRINg");
     std::string s5("this ONE contains numbers 3 4 7575");
 
-    REQUIRE(string::to_lower(s1) == "foo");
-    REQUIRE(string::to_lower(s2) == "foo");
-    REQUIRE(string::to_lower(s3) == "jonathan");
-    REQUIRE(string::to_lower(s4) == "this is another string");
-    REQUIRE(string::to_lower(s5) == "this one contains numbers 3 4 7575");
+    SECTION("using string lvalues") {
+        REQUIRE(string::to_lower(s1) == "foo");
+        REQUIRE(string::to_lower(s2) == "foo");
+        REQUIRE(string::to_lower(s3) == "jonathan");
+        REQUIRE(string::to_lower(s4) == "this is another string");
+        REQUIRE(string::to_lower(s5) == "this one contains numbers 3 4 7575");
+    }
+
+    SECTION("using string rvalues") {
+        REQUIRE(string::to_lower(std::string("foo")) == "foo");
+        REQUIRE(string::to_lower(std::string("FOO")) == "foo");
+        REQUIRE(string::to_lower(std::string("Jonathan")) == "jonathan");
+        REQUIRE(string::to_lower(std::string("THIS iS aNoTHer STRINg")) ==
+                "this is another string");
+        REQUIRE(string::to_lower(
+                    std::string("this ONE contains numbers 3 4 7575")) ==
+                "this one contains numbers 3 4 7575");
+    }
+
+    SECTION("using string constants") {
+        REQUIRE(string::to_lower("foo") == "foo");
+        REQUIRE(string::to_lower("FOO") == "foo");
+        REQUIRE(string::to_lower("Jonathan") == "jonathan");
+        REQUIRE(string::to_lower("THIS iS aNoTHer STRINg") ==
+                "this is another string");
+        REQUIRE(string::to_lower("this ONE contains numbers 3 4 7575") ==
+                "this one contains numbers 3 4 7575");
+    }
+
+    SECTION("using string views") {
+        std::string_view sv1("foo");
+        std::string_view sv2("FOO");
+        std::string_view sv3("Jonathan");
+        std::string_view sv4("THIS iS aNoTHer STRINg");
+        std::string_view sv5("this ONE contains numbers 3 4 7575");
+
+        REQUIRE(string::to_lower(sv1) == "foo");
+        REQUIRE(string::to_lower(sv2) == "foo");
+        REQUIRE(string::to_lower(sv3) == "jonathan");
+        REQUIRE(string::to_lower(sv4) == "this is another string");
+        REQUIRE(string::to_lower(sv5) == "this one contains numbers 3 4 7575");
+    }
 }
 
 TEST_CASE("string is converted to upper case") {
@@ -89,11 +287,48 @@ TEST_CASE("string is converted to upper case") {
     std::string s4("THIS iS aNoTHer STRINg");
     std::string s5("this ONE contains numbers 3 4 7575");
 
-    REQUIRE(string::to_upper(s1) == "FOO");
-    REQUIRE(string::to_upper(s2) == "FOO");
-    REQUIRE(string::to_upper(s3) == "JONATHAN");
-    REQUIRE(string::to_upper(s4) == "THIS IS ANOTHER STRING");
-    REQUIRE(string::to_upper(s5) == "THIS ONE CONTAINS NUMBERS 3 4 7575");
+    SECTION("using string lvalues") {
+        REQUIRE(string::to_upper(s1) == "FOO");
+        REQUIRE(string::to_upper(s2) == "FOO");
+        REQUIRE(string::to_upper(s3) == "JONATHAN");
+        REQUIRE(string::to_upper(s4) == "THIS IS ANOTHER STRING");
+        REQUIRE(string::to_upper(s5) == "THIS ONE CONTAINS NUMBERS 3 4 7575");
+    }
+
+    SECTION("using string rvalues") {
+        REQUIRE(string::to_upper(std::string("foo")) == "FOO");
+        REQUIRE(string::to_upper(std::string("FOO")) == "FOO");
+        REQUIRE(string::to_upper(std::string("Jonathan")) == "JONATHAN");
+        REQUIRE(string::to_upper(std::string("THIS iS aNoTHer STRINg")) ==
+                "THIS IS ANOTHER STRING");
+        REQUIRE(string::to_upper(
+                    std::string("this ONE contains numbers 3 4 7575")) ==
+                "THIS ONE CONTAINS NUMBERS 3 4 7575");
+    }
+
+    SECTION("using string constants") {
+        REQUIRE(string::to_upper("foo") == "FOO");
+        REQUIRE(string::to_upper("FOO") == "FOO");
+        REQUIRE(string::to_upper("Jonathan") == "JONATHAN");
+        REQUIRE(string::to_upper("THIS iS aNoTHer STRINg") ==
+                "THIS IS ANOTHER STRING");
+        REQUIRE(string::to_upper("this ONE contains numbers 3 4 7575") ==
+                "THIS ONE CONTAINS NUMBERS 3 4 7575");
+    }
+
+    SECTION("using string views") {
+        std::string_view sv1("foo");
+        std::string_view sv2("FOO");
+        std::string_view sv3("Jonathan");
+        std::string_view sv4("THIS iS aNoTHer STRINg");
+        std::string_view sv5("this ONE contains numbers 3 4 7575");
+
+        REQUIRE(string::to_upper(sv1) == "FOO");
+        REQUIRE(string::to_upper(sv2) == "FOO");
+        REQUIRE(string::to_upper(sv3) == "JONATHAN");
+        REQUIRE(string::to_upper(sv4) == "THIS IS ANOTHER STRING");
+        REQUIRE(string::to_upper(sv5) == "THIS ONE CONTAINS NUMBERS 3 4 7575");
+    }
 }
 
 TEST_CASE("string is split into a vector of strings") {
@@ -122,6 +357,7 @@ TEST_CASE("string is split into a vector of strings") {
 
 TEST_CASE("a string starts with another") {
     std::string s1 = "This is a string";
+    std::string_view sv1 = "This is a string";
 
     REQUIRE(string::starts_with(s1, "T") == true);
     REQUIRE(string::starts_with(s1, "This") == true);
@@ -129,10 +365,18 @@ TEST_CASE("a string starts with another") {
     REQUIRE(string::starts_with(s1, "") == false);
     REQUIRE(string::starts_with(s1, "this") == false);
     REQUIRE(string::starts_with(s1, "string") == false);
+
+    REQUIRE(string::starts_with(sv1, "T") == true);
+    REQUIRE(string::starts_with(sv1, "This") == true);
+    REQUIRE(string::starts_with(sv1, "Thou") == false);
+    REQUIRE(string::starts_with(sv1, "") == false);
+    REQUIRE(string::starts_with(sv1, "this") == false);
+    REQUIRE(string::starts_with(sv1, "string") == false);
 }
 
 TEST_CASE("a string ends with another") {
     std::string s1 = "This is a message";
+    std::string_view sv1 = "This is a message";
 
     REQUIRE(string::ends_with(s1, "e") == true);
     REQUIRE(string::ends_with(s1, "message") == true);
@@ -140,10 +384,18 @@ TEST_CASE("a string ends with another") {
     REQUIRE(string::ends_with(s1, "") == false);
     REQUIRE(string::ends_with(s1, "Message") == false);
     REQUIRE(string::ends_with(s1, "another") == false);
+
+    REQUIRE(string::ends_with(sv1, "e") == true);
+    REQUIRE(string::ends_with(sv1, "message") == true);
+    REQUIRE(string::ends_with(sv1, "something") == false);
+    REQUIRE(string::ends_with(sv1, "") == false);
+    REQUIRE(string::ends_with(sv1, "Message") == false);
+    REQUIRE(string::ends_with(sv1, "another") == false);
 }
 
 TEST_CASE("a string contains another") {
     std::string s1 = "A basic string";
+    std::string_view sv1 = "A basic string";
 
     REQUIRE(string::contains(s1, "s") == true);
     REQUIRE(string::contains(s1, "basic") == true);
@@ -151,6 +403,13 @@ TEST_CASE("a string contains another") {
     REQUIRE(string::contains(s1, "something") == false);
     REQUIRE(string::contains(s1, "") == false);
     REQUIRE(string::contains(s1, "Basic") == false);
+
+    REQUIRE(string::contains(sv1, "s") == true);
+    REQUIRE(string::contains(sv1, "basic") == true);
+    REQUIRE(string::contains(sv1, "string") == true);
+    REQUIRE(string::contains(sv1, "something") == false);
+    REQUIRE(string::contains(sv1, "") == false);
+    REQUIRE(string::contains(sv1, "Basic") == false);
 }
 
 TEST_CASE("vector of strings is joined into a string") {
